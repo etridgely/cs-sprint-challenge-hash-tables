c36ebe5d1f41326cb4cb79a7b328075304cc3af0
etridgely <5757423+etridgely@users.noreply.github.com>
2020-07-19 20:56:12 -0700

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    #create route array since that's what we'll return
+    route=[]
+
+    #transform into a dictionary for ease
+    tickets = {flight.source:flight for flight in tickets}
+
+    # set current flight
+    current = tickets["NONE"]
+
+    #knowing that the final destination is set to "NONE", loop through
+    while current.destination !="NONE":
+        #add the flight to route
+        route.append(current.destination)
+        current = tickets[current.destination]    
+
+    #add the current to the route
+    route.append(current.destination)
 
     return route
