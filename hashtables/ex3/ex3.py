b00a2027bd15f909952a4e6db0b78f286ebd0cc5
etridgely <5757423+etridgely@users.noreply.github.com>
2020-07-19 21:13:12 -0700

    ex3, done with me. here comes MVP

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,23 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    #create cache & result
+    cache = {}
+    result = []
+
+    #loop de loop, one loop through an array and then through the other
+    for i in arrays:
+        # i being an array here
+        for target in i:
+            # see if target is in the cache
+            if target in cache:
+                cache[target] += 1
+                # add the target to the result when the index == length
+                if cache[target] == len(arrays):
+                    result.append(target)
+            else:
+                #otherwise, the cache
+                cache[target] = 1
 
     return result
 
